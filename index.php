4ac652bfee09385c1de2d0258ba594d123cabee5
Nwaeze Joshua <nwaezejoshua@gmail.com>
2021-08-19 22:56:55 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Josh Resume</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-compatible" content="ie=edge">
+    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <script src="https://kit.fontawesome.com/a0e6cec4c6.js"></script>
+
+
+</head>
+
+<body class="body">
+
+
+    <div class="sidebar" id="header">
+
+
+        <div class="user">
+            <img src="josh.jpg" alt="Hng_pic" title="Hng Picture">
+            <h3 class="name">Nwaeze Joshua</h3>
+            <p class="post">Backend Developer</p>
+        </div>
+        <br>
+        <nav class="navbar">
+            <ul>
+                <li><a href="#homes"><i class="fas fa-home"></i>Home</a></li>
+                <li><a href="#about"><i class="fas fa-address-card"></i>About</a></li>
+                <li><a href="#portfolio"><i class="far fa-file"></i>Portfolio</a></li>
+                <li><a href="#workexperience"><i class="far fa-file-contract"></i>Work Experience</a></li>
+                <li><a href="#contact"><i class="far fa-envelope"></i>Contact</a></li>
+
+            </ul>
+        </nav>
+
+
+    </div>
+    <div id="menu" class="fas fa-bars"></div>
+
+
+    <section class="home" id="homes">
+        <h3>HELLO!</h3>
+        <h1>I'm <span>Nwaeze Joshua </span></h1>
+        <p>Self-taught Backend developer, skilled in writing clean codes and debugging.<br>
+            Seeking to collaborate on new technologies, new ideas and new
+            ways to solve <br>problems as a software developer.<br>
+            Created functional, buttery-smooth UIs.<br>
+            I am passionate about Web Development and Content creation
+        </p>
+
+
+        <a href="#about"><button class="btn">About me<i class="fas fa-user"></i></button></a>
+    </section>
+
+    <div id="about" class="aboutme">
+        <h1 id="heading">
+            <span>About</span>me
+        </h1>
+
+        <div class="row">
+            <div class="info">
+                <p> <span><b>Name</b> : </span>Nwaeze Joshua</p></br>
+                <p> <span><b>DOB</b> : </span>1st December</p></br>
+
+                <p> <span><b>State of Origin</b> : </span>Delta State, Nigeria
+                <p></br>
+
+                <p><span><b>Qualification</b> : </span>B.sc</p>
+            </div>
+
+            <div>
+                <a href="https://drive.google.com/file/d/1Jvbn-_mHT9ycAVrqztRr2k1IkO7p-M07/view?usp=sharing"><button class="btn" id="workexperience">Download cv <i class="fas.fa-download"></i></button></a>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <section class="portfolio">
+        <div class="resume_item resume_work">
+            <div id="portfolio">
+                <h1 id="heading">
+                    <span>Portfolio</span>
+                </h1>
+                <div class="date">2019 - Present</div>
+                <ul>
+
+                    <li>
+                        <div class="date"><a href="http://createform.netlify.app" class="link" target="_blank" rel="noopener noreferrer">Neumorphism Sign Up & Login Form Validator</a></div>
+                        <div class="info">
+                            <p> - Application of Object Oriented Programming.</p>
+                            <p> - Buttery-smooth UI.</p>
+                            <p> - Responsive web design.</p>
+                            <p class="small-text"> • HTML • CSS • Javascript</p>
+                        </div>
+                    </li>
+                    <li class="length">
+                        <div class="date"><a href="https://temperature-converter-celsius.netlify.app/" target="_blank" rel="noopener noreferrer" class="link">Temperature (celsius converter) Website</a></div>
+                        <div class="info">
+                            <p> - Easy to use, intuitive UI design.</p>
+                            <p> - Responsive UI design.</p>
+                            <p class="small-text"> • HTML • CSS • Bootstrap • Javascript • PHP</p>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="date"><a href="http://countryapis.netlify.app" class="link" target="_blank" rel="noopener noreferrer">API of Countries</a></div>
+                        <div class="info">
+                            <p> - Application of Promises.</p>
+                            <p> - Async and Await.</p>
+                            <p> - ES6+ coding style.</p>
+                            <p> - Responsive web design.</p>
+                            <p class="small-text"> • HTML • CSS • Javascript</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="date"><a href="http://24hrtime.netlify.app" class="link" target="_blank" rel="noopener noreferrer">Digital Clock</a></div>
+                        <div class="info">
+                            <p> - Application of Timing Events.</p>
+                            <p> - ES6 coding style.</p>
+                            <p> - Responsive UI design.</p>
+                            <p class="small-text"> • HTML • CSS • Javascript</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+    </section>
+    <br>
+
+    <section class="contact" id="contact">
+
+        <div id="about" class="aboutme">
+            <h1 id="heading">
+                <span>Contact</span>me
+            </h1>
+
+            <div class="row">
+                <div class="content">
+                    <span class="title">Contact Info</span>
+
+                    <div class="info">
+
+                        <span> <i class="fas fa-envelope" id="mail"></i>nwaezejoshua@gmail.com</span>
+                        <span> <i class="fas fa-phone" id="phone"></i>+2348149126243</span>
+                        <span><i class="fas fa-map-marker-alt"></i>Enugu, Nigeria </span>
+                        <p class="logos"> <img src="Hng.jpg" alt="Hng logo">
+                            <img src="Zuri logo.png" alt="Zuri logo">
+
+                        </p>
+
+
+                    </div>
+
+
+                    <table>
+                        <tr>
+                            <td>
+                                <div class="icon">
+                                    <a href="https://twitter.com/@joshclone2" target="_blank">
+                                        <i class="fab fa-twitter"></i></a>
+                                </div>
+                                <div class="data">
+                                    <p class=""><a href="https://twitter.com/@joshclone2" target="_blank" class="link">Twitter</a></p>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="icon">
+                                    <a href="https://github.com/joshclone" target="_blank">
+                                        <i class="fab fa-github"></i></a>
+                                </div>
+                                <div class="data">
+                                    <p class=""><a href="https://github.com/joshclone" target="_blank" class="link">Github</a></p>
+                                </div>
+
+                            </td>
+                            <td>
+                                <div class="icon">
+                                    <a href="https://linkedin.com/in/joshuanwaeze" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                                </div>
+                                <div class="data">
+                                    <p class=""><a href="https://linkedin.com/in/joshuanwaeze" target="_blank" class="link">Linked In</a>
+                                    </p>
+                                </div>
+
+                            </td>
+                        </tr>
+                    </table>
+
+
+
+
+
+
+
+
+
+                </div>
+            </div>
+
+            <div class="action">
+
+                <form action="mail.php" method="post">
+
+                    <input type="text" name="name" placeholder="Your name.." class="box">
+
+
+                    <input type="email" name="email" placeholder="Your email.." class="box">
+
+
+                    <textarea name="message" placeholder="Write something.." class="box" style="height:200px"></textarea>
+
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+
+    </section>
+
+
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="main.js"></script>
+</body>
